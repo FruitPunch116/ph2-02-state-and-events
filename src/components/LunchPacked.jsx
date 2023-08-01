9503a94328a2e8b7fdfc2c2f70d290bb82e7bea9
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-01 12:35:48 -0400

    toggle for lunch

--- src/components/LunchPacked.jsx
+++ src/components/LunchPacked.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
 function LunchPacked() {
 
+  const [ lunch, setLunch ] = useState( false )
+  
+  function togglePacked () {
+    setLunch(!lunch)
+  }
+
   return (
-    <button>Lunch Not Packed</button>
+    <button onClick={togglePacked} >{lunch ? "Enjoy your lunch" : "Lunch Not Packed"}</button>
   )
 
 }
