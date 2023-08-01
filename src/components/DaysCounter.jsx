893d4c838fe1d47fedc0ccef289dba4dbb5b1b4b
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-01 12:32:07 -0400

    classname switch

--- src/components/DaysCounter.jsx
+++ src/components/DaysCounter.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react"
+
 function DaysCounter() {
 
-  const numDays = 7
+  const [ numDays, setNumDays ] = useState( 7 )
+
+  function handleDays () {
+   setNumDays(numDays - 1)
+  }
 
   return (
 
-    <button>{numDays} Days Until School Starts!</button>
+    <button onClick={handleDays} >{numDays > 0 ? `${numDays} Days Until School Starts!` : "School has started"} </button>
 
   )
 
